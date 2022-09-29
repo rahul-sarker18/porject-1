8e78c8c25617350e808bbbbb662bc985a28b2009
rahul-sarker18 <rahulsarker1495@gmail.com>
2022-09-29 13:33:06 +0530

    bonas qurtion add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Bonas from './components/Bonas/Bonas';
 import Data from './components/DataFech/Data';
 
 function App() {
   return (
     <div className="App">
       <Data></Data>
+      <Bonas></Bonas>
     </div>
   );
 }
